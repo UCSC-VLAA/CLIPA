819dffb86aa076dd68c289984f6d9874759b0e24
lixianhang <xianhangli@knights.ucf.edu>
2023-10-03 12:02:08 -0700

    env update

--- clipa_jax/scripts/tools/prepare_env.sh
+++ clipa_jax/scripts/tools/prepare_env.sh
@@ -4,9 +4,15 @@ cd ~/CLIPA/clipa_jax/
 python3 -m venv bv_venv
 . ~/CLIPA/clipa_jax/bv_venv/bin/activate
 
+pip uninstall jax -y
+pip uninstall jaxlib -y
+pip uninstall tensorflow  -y
+
+
+
 pip install -U pip  # Yes, really needed.
   # NOTE: doesn't work when in requirements.txt -> cyclic dep
-pip install "jax[tpu]>=0.2.16" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+pip install "jax[tpu]==0.4.6" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 pip install -r requirements-tpu.txt
 pip install -U nltk
 pip install torch==1.12.1+cpu torchvision==0.13.1+cpu torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cpu
